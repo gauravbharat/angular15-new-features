92e94a2f82e1406035992d58bcecdaed624e4733
Gaurav Bharat <gaurav.mendse@gmail.com>
2023-04-12 22:07:56 +0530

    standalone component and directive

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DetailsComponent } from "./details/details.component";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
 })
 export class WelcomeComponent {}
